0733af073bd1963d8e54d21e56b460ecd755a7e1
Steven Gonsalvez <steven.gonsalvez@gmail.com>
2025-01-15 00:39:10 +0000

    fix: vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 
@@ -11,6 +12,7 @@ function App() {
           {/* Add more routes as needed */}
         </Route>
       </Routes>
+      <Analytics />
     </Router>
   );
 }
