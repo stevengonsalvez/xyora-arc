489334f7a0c215c82ef6dc51e216219be05a4227
steven gonsalvez <steven.gonsalvez@gmail.com>
2025-05-03 15:36:42 +0100

    feat: add About page route to the application

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
           {/* Add more routes as needed */}
         </Route>
       </Routes>
